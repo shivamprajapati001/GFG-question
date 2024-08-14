1cd3676706f6b3ecc3a2e2b8685660835d8fcd73
shivamprajapati001 <523110023@nitkkr.ac.in>
2024-08-14 22:37:09 +0530

    Create HW_Area of a circle 6.java

---
+++ HW_Area of a circle 6.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        x=x/2;
+        int Area;
+         Area=(22*x*x)/7;
+
+
+            System.out.print(Area);
+    }
+}
