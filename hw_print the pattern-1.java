8bd1f55e1f32b117bb781412bc13b177f5a18263
shivamprajapati001 <523110023@nitkkr.ac.in>
2024-08-14 21:28:48 +0530

    Create hw_print the pattern-1.java

---
+++ hw_print the pattern-1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         System.out.println("Hello");
+         System.out.println("I");
+         System.out.println("am");
+         System.out.println("enjoying");
+         System.out.println("coding");
+    }
+}
