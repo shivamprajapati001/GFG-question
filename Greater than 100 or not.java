b4bac2f530458f3c69b897447c1b706fef17a995
shivamprajapati001 <523110023@nitkkr.ac.in>
2024-08-14 22:18:00 +0530

    Create Greater than 100 or not.java

---
+++ Greater than 100 or not.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>100)
+            System.out.print("True");
+        else
+            System.out.print("False");
+    }
+}
