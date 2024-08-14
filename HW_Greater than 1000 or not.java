5fe5988f264ff99ead7278731cdd0fcecc4c82ea
shivamprajapati001 <523110023@nitkkr.ac.in>
2024-08-14 22:27:08 +0530

    Create HW_Greater than 1000 or not.java

---
+++ HW_Greater than 1000 or not.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+              Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+         if(x>1000)
+            System.out.print("True");
+        else
+            System.out.print("False");
+    }
+}
