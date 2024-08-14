1cb82cb2221f5390e66b5f082fd42e6b208ae85a
shivamprajapati001 <523110023@nitkkr.ac.in>
2024-08-14 22:42:08 +0530

    Create HW_Area of triangle 13.java

---
+++ HW_Area of triangle 13.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner sc = new Scanner(System.in);
+       double x=sc.nextDouble();
+    double y=sc.nextDouble();
+         double s=(x*y)/2;
+        System.out.print(s);
+        
+    }
+}
