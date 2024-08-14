3660cf96c45842925849fc3dc8f7aea1e0c3f765
shivamprajapati001 <523110023@nitkkr.ac.in>
2024-08-14 22:01:40 +0530

    Create Fahrenheit and Celsius.java

---
+++ Fahrenheit and Celsius.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here
+        . Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanener(System.in);
+            double f=sc.nextDouble(); 
+        double c;
+        c=[(f-32)*5]/9;
+            System.out.println(c);
+    }
+}
