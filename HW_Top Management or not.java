f3ececa22ccfa3e17c143435d4fbbd78e609493f
shivamprajapati001 <523110023@nitkkr.ac.in>
2024-08-15 12:47:33 +0530

    Create HW_Top Management or not.java

---
+++ HW_Top Management or not.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+            int b= sc.nextInt();
+   int c= sc.nextInt();
+  if(a>=10||b>=50000||c>=10)
+      System.out.print("You are in top management");
+    else
+    System.out.print("You are not in top management");
+
+    }
+}
